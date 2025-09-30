4e32de6d0bf14197e883c383f560c6646932abb6
Lokesh Reddy <122959837+KTM0SPARROW@users.noreply.github.com>
2025-09-30 17:25:14 +0530

    MaxElement.java

---
+++ 30-09-2025 sliding window/MaxElement.java
@@ -0,0 +1,9 @@
+Problem Statement:
+   list all the maximum elements in every window of size in the given array
+Approaches:
+  1-->Naive approach-->using two for loops -->T.C-->O(n*k)--> S.C-->O(n)
+
+  2-->using Max Heap-->O(n*k)
+
+  3-->optimal Approach-->using de
+  
